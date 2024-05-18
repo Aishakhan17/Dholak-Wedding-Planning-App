6010a0df63bf444886d3762d4388f08ffed48a78
Aishakhan17 <aishakhan_19@hotmail.com>
2024-05-19 03:07:01 +0500

    update

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from "../../components/Navbar"
+import ImageCarousel from '../../components/ImageCarousel'
 
 const Landing = () => {
-  return (
-    <div>
-        <Navbar />
-        <h4 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Landing</h4>
-    </div>
-  )
+
+    let images = []
+    return (
+        <div className='h-full min-h-screen mx-auto w-5/6'>
+            <Navbar />
+            <h4 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">No Panic at the Dholak</h4>
+            <div className='justify-center'>
+                <ImageCarousel images={images}/>
+            </div>
+            <div className='mt-10'>
+                <h4 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Why use a wedding planner?</h4>
+            </div>
+        </div>
+    )
 }
 
 export default Landing
